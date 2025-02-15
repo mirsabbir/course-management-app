280892fb59c2e38bf80ae54c7713bad21ec64647
Mir Sabbir Alam <mirsabbiralam@gmail.com>
2025-02-16 00:46:14 +0600

    add duende quickstart UI

--- backend/auth-service/src/AuthService/Program.cs
+++ backend/auth-service/src/AuthService/Program.cs
@@ -1,8 +1,38 @@
+using AuthService;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+// Register the ApplicationDbContext with SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(""));
+
+// Add ASP.NET Core Identity with Entity Framework stores
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+})
+.AddInMemoryIdentityResources(Config.IdentityResources)
+.AddInMemoryApiScopes(Config.ApiScopes)
+.AddInMemoryClients(Config.Clients)
+.AddTestUsers(Config.Users)
+.AddDeveloperSigningCredential();
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddControllersWithViews();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -14,10 +44,28 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+// Redirect HTTP to HTTPS
 app.UseHttpsRedirection();
 
+// Serve static files (CSS, JS, images, etc.)
+app.UseStaticFiles();
+
+// Enable routing
+app.UseRouting();
+
+// Add IdentityServer middleware (to enable the endpoints e.g. /connect/token)
+app.UseIdentityServer();
+
+// Enable authentication (ASP .NET Identity) 
+app.UseAuthentication();
+
+// Enable authorization
 app.UseAuthorization();
 
+// Map Razor Pages (for Quickstart UI)
+app.MapRazorPages();
+
+// Map controllers (for API endpoints)
 app.MapControllers();
 
 app.Run();
---
+++ backend/auth-service/src/AuthService/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthService
+{
+    public class User : IdentityUser
+    {
+        // Add additional properties here if needed
+    }
+}
